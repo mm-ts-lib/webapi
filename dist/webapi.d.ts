ab1b20e21a6ba007d9d6704f2790c869070b1dd6
zhufeng <zhfjyq@163.com>
2018-04-15 23:01:18 +0800

    OK

--- dist/webapi.d.ts
+++ dist/webapi.d.ts
@@ -1,36 +1,4 @@
-/**
- * API类型
- */
-export declare type API_TYPE = 'WS' | 'HTTP';
-/**
- * api 分组
- */
-export interface IAPI_GROUP {
-    [x: string]: {
-        [x: string]: any;
-    };
-}
-/**
- * api 基础类型
- */
-export interface IAPI_BASE {
-    (req: any): any;
-    on: (req: any) => any;
-    type: API_TYPE;
-}
-/**
- * 标准api请求
- */
 export interface IAPI<REQ_T, RES_T> {
     (req: REQ_T): Promise<RES_T>;
-    on: (req: REQ_T) => Promise<RES_T>;
-    type: API_TYPE;
 }
-/**
- * 缺省 http Json请求
- */
 export declare function HTTP<REQ_T, RES_T>(): IAPI<REQ_T, RES_T>;
-/**
- * 缺省ws请求
- */
-export declare function WS<REQ_T, RES_T>(): IAPI<REQ_T, RES_T>;
--- dist/webapi.js
+++ dist/webapi.js
@@ -1,33 +1,12 @@
 "use strict";
+// /**
+//  * API类型
+//  */
+// export type API_TYPE = 'WS' | 'HTTP';
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * 缺省 http Json请求
- */
 function HTTP() {
-    const func = (req) => {
-        throw new Error('INVALID HTTP API');
-    };
-    const http = func;
-    http.type = 'HTTP';
-    http.on = (req) => {
-        throw new Error('HTTP API no on FUNC');
-    };
-    return http;
+    const func = () => { throw new Error('INVALID HTTP API'); };
+    return func;
 }
 exports.HTTP = HTTP;
-/**
- * 缺省ws请求
- */
-function WS() {
-    const func = (req) => {
-        throw new Error('INVALID WS API');
-    };
-    const ws = func;
-    ws.type = 'WS';
-    ws.on = (req) => {
-        throw new Error('INVALID WS on API');
-    };
-    return ws;
-}
-exports.WS = WS;
 //# sourceMappingURL=webapi.js.map
